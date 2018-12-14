9eefd3f285ce286b2c9007a64b5ab0a922f35e72
Jese Rodriguez <hi@jese.me>
2018-12-14 15:42:42 -0400

    Set Buffer as global in UdpSockets.js
    For interop with https://github.com/netbeast/react-native-ssdp

--- UdpSockets.js
+++ UdpSockets.js
@@ -6,6 +6,8 @@
 
 var UdpSocket = require('./UdpSocket');
 
+global.Buffer = global.Buffer || require('buffer').Buffer
+
 exports.createSocket = function(options) {
   if (typeof options === 'string') options = { type: options }
   return new UdpSocket(options)
